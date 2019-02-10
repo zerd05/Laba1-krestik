b156dfc56d6e122ee1c0382e509b06cb7a9f5d65
zerd <zerd@MacBook-Pro-zerd-6.local>
2019-02-10 18:31:41 +0300

    Initial commit

---
+++ app/src/main/java/inc/zerd/laba1/mainActivity.java
@@ -0,0 +1,15 @@
+package inc.zerd.laba1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class mainActivity extends Activity {
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.mainactivity);
+    }
+}
